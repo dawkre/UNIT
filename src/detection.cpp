ad9c467c9046e14455410d6bddc3b516719d682c
dawkre <dawid.kreczkowski@gmail.com>
2018-07-14 23:38:37 +0200

    detection cleanup; move detection initialization to function

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,26 +1,29 @@
 #include "detection.h"
 #include <stdio.h>
 
-bbox detect_object( Mat frame, CascadeClassifier face_cascade )
+bbox detect_object( Mat frame, CascadeClassifier cascade )
 {
-	std::vector<Rect> faces;
+	std::vector<Rect> objects;
 
 	equalizeHist( frame, frame );
 
-	face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-	bbox detection ;
-	if (  faces.size() > 0)
+	cascade.detectMultiScale( frame, objects, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+	bbox detection = {-1,-1,-1,-1};
+	if (  objects.size() > 0)
 	{
 		detection = {
-			faces[0].x,
-			faces[0].y,
-			faces[0].width,
-			faces[0].height
+			objects[0].x,
+			objects[0].y,
+			objects[0].width,
+			objects[0].height
 		};
-
-	} else {
-		detection = {0, 0, 0, 0};
 	}
 
 	return detection;
+}
+
+CascadeClassifier initialize_detector(String cascade_name) {
+	CascadeClassifier cascade;
+	if ( !cascade.load( cascade_name ) ) { printf("detector initialization failed\n"); throw - 1; };
+	return cascade;
 }
--- src/detection.h
+++ src/detection.h
@@ -1,3 +1,9 @@
+/*
+The detection files contain all detection-related logic.
+The detect_object method should return a bbox object that describes
+the bounding box around the detected object.
+*/
+
 #include "structs.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
@@ -6,3 +12,4 @@
 using namespace cv;
 
 bbox detect_object( Mat frame, CascadeClassifier face_cascade );
+CascadeClassifier initialize_detector(String cascade_name);
